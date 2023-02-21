9533e1effcd2787848e454eaa4a12c952e67b863
onur <onurulusoys4@gmail.com>
2023-02-21 16:21:01 +0300

    Done parent class (gpio) driver behavior unit tests

--- driver/driver-googletest/test.cpp
+++ driver/driver-googletest/test.cpp
@@ -7,8 +7,9 @@
 using namespace DeviceSim;
 
 int counter = 0;
-vector<std::string> filenames = {"test_device", "log", "command", "communication-register"};
+vector<std::string> filenames = {"test_device", "log", "command", "communication-register", "temp"};
 void deleteGarbage(vector<string> filenames);
+bool areFilesEqual(const std::string& filePath1, const std::string& filePath2);
 
 TEST(GPIOTest, ConstructorTest) {
     // Test constructor by creating a new instance of the class
@@ -173,6 +174,89 @@ TEST(GPIOTest, WriteTest) {
     
 }
 
+TEST(GPIOTest, ShowTest) {
+    char* dev_name = generateDevName("test_device");
+    GPIO_Device gpio(dev_name);
+
+    gpio.devContent.config(DEFAULT, &gpio);
+
+    gpio.devContent.show(&gpio);
+    //It's gonna generate temp file
+
+    ASSERT_TRUE(areFilesEqual("temp", "defaultConfigShow"));
+    
+    deleteGarbage(filenames);
+}
+
+TEST(GPIOTest, ParseTest) {
+    //GPIO Parse Method Test
+    char* dev_name = generateDevName("test_device");
+    GPIO_Device gpio(dev_name);
+
+    // Open device
+    gpio.device_open(WRITEONLY, &gpio);
+
+    string json_file_dir = "dev/default_GPIO_chipInfo.json";
+    // Call the function to be tested
+    gpio.parse(json_file_dir, &gpio);
+    gpio.device_close();
+
+    // Assert that the values are correctly parsed
+    EXPECT_EQ("-", gpio.devContent.read(10, "name", &gpio));
+    EXPECT_EQ("-", gpio.devContent.read(10, "consumer", &gpio));
+    EXPECT_EQ("[INPUT]", gpio.devContent.read(10, "FLAG_IS_OUT", &gpio));
+    EXPECT_EQ("[ACTIVE_HIGH]", gpio.devContent.read(10, "FLAG_ACTIVE_LOW", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(10, "FLAG_OPEN_DRAIN", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(10, "FLAG_OPEN_SOURCE", &gpio));
+    EXPECT_EQ("[]", gpio.devContent.read(10, "FLAG_KERNEL", &gpio));
+
+    EXPECT_EQ("-", gpio.devContent.read(20, "name", &gpio));
+    EXPECT_EQ("-", gpio.devContent.read(20, "consumer", &gpio));
+    EXPECT_EQ("[INPUT]", gpio.devContent.read(20, "FLAG_IS_OUT", &gpio));
+    EXPECT_EQ("[ACTIVE_HIGH]", gpio.devContent.read(20, "FLAG_ACTIVE_LOW", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(20, "FLAG_OPEN_DRAIN", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(20, "FLAG_OPEN_SOURCE", &gpio));
+    EXPECT_EQ("[]", gpio.devContent.read(20, "FLAG_KERNEL", &gpio));
+    
+    // SPI Parse Method Test
+    SPI_Device spi(dev_name);
+
+    // Open device
+    spi.device_open(WRITEONLY, &spi);
+
+    json_file_dir = "dev/default_SPI_chipInfo.json";
+    // Call the function to be tested
+    spi.parse(json_file_dir, &spi);
+    spi.device_close();
+
+    // Assert that the values are correctly parsed
+    EXPECT_EQ("SPI_0", spi.devContent.read(0, "name", &spi));
+    EXPECT_EQ("-", spi.devContent.read(0, "consumer", &spi));
+    EXPECT_EQ("0", spi.devContent.read(0, "cpol", &spi));
+    EXPECT_EQ("0", spi.devContent.read(0, "cpha", &spi));
+    EXPECT_EQ("false", spi.devContent.read(0, "lsb_first", &spi));
+    EXPECT_EQ("false", spi.devContent.read(0, "cs_high", &spi));
+    EXPECT_EQ("false", spi.devContent.read(0, "3wire", &spi));
+    EXPECT_EQ("false", spi.devContent.read(0, "loopback", &spi));
+
+
+    EXPECT_EQ("SPI_1", spi.devContent.read(1, "name", &spi));
+    EXPECT_EQ("-", spi.devContent.read(1, "consumer", &spi));
+    EXPECT_EQ("0", spi.devContent.read(1, "cpol", &spi));
+    EXPECT_EQ("0", spi.devContent.read(1, "cpha", &spi));
+    EXPECT_EQ("false", spi.devContent.read(1, "lsb_first", &spi));
+    EXPECT_EQ("false", spi.devContent.read(1, "cs_high", &spi));
+    EXPECT_EQ("false", spi.devContent.read(1, "3wire", &spi));
+    EXPECT_EQ("false", spi.devContent.read(1, "loopback", &spi));
+
+    //deleteGarbage(filenames);
+    
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
 void deleteGarbage(vector<string> filenames){
     // iterate over the filenames and delete each file
@@ -182,7 +266,34 @@ void deleteGarbage(vector<string> filenames){
     }
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+bool areFilesEqual(const std::string& filePath1, const std::string& filePath2) {
+    std::ifstream file1(filePath1, std::ios::binary);
+    std::ifstream file2(filePath2, std::ios::binary);
+
+    if (!file1 || !file2) {
+        // Failed to open one or both files
+        return false;
+    }
+
+    // Read the files into two strings
+    std::stringstream buffer1, buffer2;
+    buffer1 << file1.rdbuf();
+    buffer2 << file2.rdbuf();
+    std::string contents1 = buffer1.str();
+    std::string contents2 = buffer2.str();
+
+    // Replace end-of-line characters with a common format
+    for (char& c : contents1) {
+        if (c == '\r') {
+            c = '\n';
+        }
+    }
+    for (char& c : contents2) {
+        if (c == '\r') {
+            c = '\n';
+        }
+    }
+
+    // Compare the strings
+    return contents1 == contents2;
 }
