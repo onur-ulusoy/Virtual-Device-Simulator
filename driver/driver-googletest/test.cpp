ee7a4b99d8d6df30af6d9a65dde09cca7ed5a463
onur <onurulusoys4@gmail.com>
2023-02-21 13:02:19 +0300

    Write config and write tests from GPIOTest

--- driver/driver-googletest/test.cpp
+++ driver/driver-googletest/test.cpp
@@ -93,7 +93,7 @@ TEST(GPIOTest, ParseGPIOTest) {
     parse_GPIO(json_file_dir, &gpio);
     gpio.device_close();
 
-    // Assert that the values are correctly read from the file  
+    // Assert that the values are correctly parsed
     EXPECT_EQ("-", gpio.devContent.read(10, "name", &gpio));
     EXPECT_EQ("-", gpio.devContent.read(10, "consumer", &gpio));
     EXPECT_EQ("[INPUT]", gpio.devContent.read(10, "FLAG_IS_OUT", &gpio));
@@ -109,6 +109,65 @@ TEST(GPIOTest, ParseGPIOTest) {
     EXPECT_EQ("[...]", gpio.devContent.read(20, "FLAG_OPEN_DRAIN", &gpio));
     EXPECT_EQ("[...]", gpio.devContent.read(20, "FLAG_OPEN_SOURCE", &gpio));
     EXPECT_EQ("[]", gpio.devContent.read(20, "FLAG_KERNEL", &gpio));
+    
+    deleteGarbage(filenames);
+    
+}
+
+TEST(GPIOTest, ConfigTest) {
+    char* dev_name = generateDevName("test_device");
+    GPIO_Device gpio(dev_name);
+
+    // Open device
+    //gpio.device_open(WRITEONLY, &gpio);
+
+    //string json_file_dir = "dev/default_GPIO_chipInfo.json";
+    // Call the function to be tested
+    
+    gpio.devContent.config(DEFAULT, &gpio);
+
+    // Assert that the values are correctly configurated
+    EXPECT_EQ("-", gpio.devContent.read(10, "name", &gpio));
+    EXPECT_EQ("-", gpio.devContent.read(10, "consumer", &gpio));
+    EXPECT_EQ("[INPUT]", gpio.devContent.read(10, "FLAG_IS_OUT", &gpio));
+    EXPECT_EQ("[ACTIVE_HIGH]", gpio.devContent.read(10, "FLAG_ACTIVE_LOW", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(10, "FLAG_OPEN_DRAIN", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(10, "FLAG_OPEN_SOURCE", &gpio));
+    EXPECT_EQ("[]", gpio.devContent.read(10, "FLAG_KERNEL", &gpio));
+
+    EXPECT_EQ("-", gpio.devContent.read(20, "name", &gpio));
+    EXPECT_EQ("-", gpio.devContent.read(20, "consumer", &gpio));
+    EXPECT_EQ("[INPUT]", gpio.devContent.read(20, "FLAG_IS_OUT", &gpio));
+    EXPECT_EQ("[ACTIVE_HIGH]", gpio.devContent.read(20, "FLAG_ACTIVE_LOW", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(20, "FLAG_OPEN_DRAIN", &gpio));
+    EXPECT_EQ("[...]", gpio.devContent.read(20, "FLAG_OPEN_SOURCE", &gpio));
+    EXPECT_EQ("[]", gpio.devContent.read(20, "FLAG_KERNEL", &gpio));
+    
+    deleteGarbage(filenames);
+    
+}
+
+TEST(GPIOTest, WriteTest) {
+    char* dev_name = generateDevName("test_device");
+    GPIO_Device gpio(dev_name);
+
+    gpio.devContent.config(DEFAULT, &gpio);
+
+    gpio.devContent.write(1, "name", "test_name", &gpio);
+    string test_data = gpio.devContent.read(1, "name", &gpio);
+    // Assert that the values are correctly written to the file  
+    EXPECT_EQ("test_name", test_data);
+
+    gpio.devContent.write(7, "consumer", "test_consumer", &gpio);
+    test_data = gpio.devContent.read(7, "consumer", &gpio);
+    // Assert that the values are correctly written to the file  
+    EXPECT_EQ("test_consumer", test_data);
+
+    gpio.devContent.write(10, "FLAG_ACTIVE_LOW", "[ACTIVE_LOW]", &gpio);
+    test_data = gpio.devContent.read(10, "FLAG_ACTIVE_LOW", &gpio);
+    // Assert that the values are correctly written to the file  
+    EXPECT_EQ("[ACTIVE_LOW]", test_data);
+
     
     deleteGarbage(filenames);
     
