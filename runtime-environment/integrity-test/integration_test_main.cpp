7404e0a38e2d37b5d5569e0f47c1c1206b867fd4
onur <onurulusoys4@gmail.com>
2023-02-28 17:00:39 +0300

    Refactore integration tests into separate libraries and main.

---
+++ runtime-environment/integrity-test/integration_test_main.cpp
@@ -0,0 +1,9 @@
+#include "lib-integrationtest.hpp"
+
+using namespace IntegrationTest;
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+}
---
+++ runtime-environment/integrity-test/lib-integrationtest.hpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <gtest/gtest.h>
+#include "libdriver.hpp"
+#include "libtesterutility.hpp"
+
+using namespace DeviceSim;
+//namespace Tests-Vds{
+namespace IntegrationTest{
+
+    enum ComparisonMode {
+            LOG,
+            REGISTER_LOG,
+            DEFAULT_MODE
+    };
+
+    bool areFilesEqual(const string& filePath1, const string& filePath2, ComparisonMode mode);
+    void deleteGarbage(vector<string> filenames);
+  
+}
