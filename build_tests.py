27100cfa4eebbf9c8c49a1fe2170c61c6c2e515e
onur <onurulusoys4@gmail.com>
2023-03-02 10:58:27 +0300

    Edit build and run test scripts in root folder to take parameters

---
+++ build_tests.py
@@ -0,0 +1,42 @@
+import sys
+from os import chdir
+
+def build_utests():
+    sys.path.append('driver/driver-unit-test')
+    import build_utests
+    chdir("driver/driver-unit-test")
+    build_utests.main()
+    chdir("../../..")
+
+def build_itests():
+    sys.path.append('runtime-environment/integrity-test')
+    import build_itests
+    chdir("runtime-environment/integrity-test")
+    build_itests.main()
+
+def argument_error():
+    print("***************************")
+    print("Example:")
+    print("python3 build_tests allunittests")
+    print("python3 build_tests allintegrationtests")
+    print("python3 build_tests alltests")
+    print("***************************")
+    exit()
+
+def main():
+    if len(sys.argv) <= 1:
+        print("Argument must be passed")
+        argument_error()
+    elif sys.argv[1] == "allunittests":
+        build_utests()
+    elif sys.argv[1] == "allintegrationtests":
+        build_itests()
+    elif sys.argv[1] == "alltests":
+        build_utests()
+        build_itests()
+    else:
+        print("Argument is invalid")
+        argument_error()
+
+if __name__ == "__main__":
+    main()
---
+++ run_tests.py
@@ -0,0 +1,42 @@
+import sys
+from os import chdir
+
+def run_utests():
+    sys.path.append('driver/driver-unit-test')
+    import run_utests
+    chdir("driver/driver-unit-test")
+    run_utests.main()
+    chdir("../../")
+
+def run_itests():
+    sys.path.append('runtime-environment/integrity-test')
+    import run_itests
+    chdir("runtime-environment/integrity-test")
+    run_itests.main()
+
+def argument_error():
+    print("***************************")
+    print("Example:")
+    print("python3 run_tests allunittests")
+    print("python3 run_tests allintegrationtests")
+    print("python3 run_tests alltests")
+    print("***************************")
+    exit()
+
+def main():
+    if len(sys.argv) <= 1:
+        print("Argument must be passed")
+        argument_error()
+    elif sys.argv[1] == "allunittests":
+        run_utests()
+    elif sys.argv[1] == "allintegrationtests":
+        run_itests()
+    elif sys.argv[1] == "alltests":
+        run_utests()
+        run_itests()
+    else:
+        print("Argument is invalid")
+        argument_error()
+
+if __name__ == "__main__":
+    main()
