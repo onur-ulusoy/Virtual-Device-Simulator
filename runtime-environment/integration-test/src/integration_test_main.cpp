3951861a5bd669b116d2910c1eb63ac3675ce119
Onur Ulusoy <onurulusoys4@gmail.com>
2023-03-11 18:29:28 +0300

    Refactor integration test main so that it returns results of each test in a vector

--- runtime-environment/integration-test/src/integration_test_main.cpp
+++ runtime-environment/integration-test/src/integration_test_main.cpp
@@ -20,7 +20,7 @@ TEST(IntegrationTest, Case4){
 } 
 
 
-int main(int argc, char **argv) {
+vector<int> main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
 
     
@@ -31,5 +31,36 @@ int main(int argc, char **argv) {
 
     deleteGarbage(new_files);
     cout << "End of tests" << endl;
-    return result;
+
+    // create an array to store the test results
+    std::vector<int> test_results;
+
+    // iterate over the tests and store the results in the array
+    const ::testing::UnitTest* const unit_test = ::testing::UnitTest::GetInstance();
+    for (int i = 0; i < unit_test->total_test_case_count(); i++) {
+        const ::testing::TestCase* const test_case = unit_test->GetTestCase(i);
+        for (int j = 0; j < test_case->total_test_count(); j++) {
+            const ::testing::TestInfo* const test_info = test_case->GetTestInfo(j);
+            if (test_info->result()->Failed()) {
+                // test failed, add -1 to the array
+                test_results.push_back(-1);
+            } else {
+                // test passed, add 1 to the array
+                test_results.push_back(1);
+            }
+        }
+    }
+
+    // print the test results array
+    std::cout << "[";
+    for (int i = 0; i < test_results.size(); i++) {
+        std::cout << test_results[i];
+        if (i < test_results.size() - 1) {
+            std::cout << " ";
+        }
+    }
+    std::cout << "]" << std::endl;
+    
+
+    return test_results;
 }
