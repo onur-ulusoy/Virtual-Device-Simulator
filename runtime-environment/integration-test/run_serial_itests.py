b66d30bd42606a8a0958b2056bb3f53ed8f2214b
Onur Ulusoy <onurulusoys4@gmail.com>
2023-03-11 23:07:29 +0300

    Enhence serial test script for improved readability and visuality

--- runtime-environment/integration-test/run_serial_itests.py
+++ runtime-environment/integration-test/run_serial_itests.py
@@ -1,4 +1,5 @@
 import subprocess, sys, csv
+from termcolor import colored
 
 if len(sys.argv) < 2:
     print("Please give test count as an argument")
@@ -17,20 +18,14 @@ test_count = int(test_count)
 
 # Define the columns to print
 all_columns = [[""], [""], [""], [""]]
-test_set = 4
+test_set = 4    
+fail_count = 0
+success_count = 0
 
-# Print the columns
-""" while True:
-    # Clear the console
-    #os.system('cls' if os.name == 'nt' else 'clear')
-    # Wait for 1 second
-    os.system('cls' if os.name == 'nt' else 'clear') """
-    
-    
 import os
 
-for i in range(0, test_count):
-
+for iter_count in range(0, test_count):
+    
     # Use subprocess to start the process in the background
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -41,7 +36,7 @@ for i in range(0, test_count):
 
     # Check the return code of the process
     if process.returncode == 0:
-        print("Serial tests {} x {} = {} completed successfully.\n".format(i+1, test_set, (i+1)*test_set))
+        print("Serial tests {} x {} = {} out of {} completed successfully.\n".format(iter_count+1, test_set, (iter_count+1)*test_set, test_count*test_set))
     else:
         print("Process failed with return code:", process.returncode)
 
@@ -52,10 +47,30 @@ for i in range(0, test_count):
         for column in columns:
             i = 0
             while (i < len(column)):
-                    #print(column[i])
+                if column[i] == "✓":
+                    all_columns[i].append(colored(column[i], "green"))
+                    success_count += 1
+                elif column[i] == "✗":
+                    all_columns[i].append(colored(column[i], "red"))
+                    fail_count += 1
+                else:
                     all_columns[i].append(column[i])
-                    print(" ".join(all_columns[i]))
-                    i += 1
+                print(" ".join(all_columns[i]))
+                i += 1
                     
         print()
+
+    print("{}/{} of integration tests is successful.\n".format(success_count, success_count+fail_count))
+
+    if iter_count == test_count - 1:
+        with open("test_results_serial.txt", "w") as file:
+            file.write("{}/{} of integration tests is successful.\n".format(success_count, success_count+fail_count))
+
+        
+
+
+
+
+            
+
     
