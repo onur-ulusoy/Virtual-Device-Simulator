e4d2a299c45e4c124cbf9a1147e2651dec1ce548
Onur Ulusoy <onurulusoys4@gmail.com>
2023-03-11 22:07:08 +0300

    Enhance run_serial_tests.py to display results in a clean, easy-to-read column layout

--- runtime-environment/integration-test/run_serial_itests.py
+++ runtime-environment/integration-test/run_serial_itests.py
@@ -1,4 +1,5 @@
-import subprocess, sys
+import subprocess, sys, csv
+
 if len(sys.argv) < 2:
     print("Please give test count as an argument")
     exit()
@@ -14,15 +15,47 @@ cmd = ["python3", "run_itests.py"]
 
 test_count = int(test_count)
 
+# Define the columns to print
+all_columns = [[""], [""], [""], [""]]
+test_set = 4
+
+# Print the columns
+""" while True:
+    # Clear the console
+    #os.system('cls' if os.name == 'nt' else 'clear')
+    # Wait for 1 second
+    os.system('cls' if os.name == 'nt' else 'clear') """
+    
+    
+import os
+
 for i in range(0, test_count):
+
     # Use subprocess to start the process in the background
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Wait for the process to complete
     process.wait()
 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     # Check the return code of the process
     if process.returncode == 0:
-        print("Serial test {} completed successfully.".format(i+1))
+        print("Serial tests {} x {} = {} completed successfully.\n".format(i+1, test_set, (i+1)*test_set))
     else:
         print("Process failed with return code:", process.returncode)
+
+    # Read the contents of the text file and print each column
+    with open("test_results.txt") as f:
+        reader = csv.reader(f, delimiter='\t')
+        columns = zip(*reader)
+        for column in columns:
+            i = 0
+            while (i < len(column)):
+                    #print(column[i])
+                    all_columns[i].append(column[i])
+                    print(" ".join(all_columns[i]))
+                    i += 1
+                    
+        print()
+    
