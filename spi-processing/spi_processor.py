87859892bf3bbdefb1bfe1f27e15f9f363988f82
Onur Ulusoy <onurulusoys4@gmail.com>
2023-04-07 14:13:41 +0300

    Modify the database schema to include an entry_count column and make script to create different table for each unique key/value pair

--- spi-processing/spi_processor.py
+++ spi-processing/spi_processor.py
@@ -4,6 +4,7 @@ import argparse
 import sqlite3
 from cryptography.fernet import Fernet
 from base64 import urlsafe_b64encode
+import hashlib
 
 class SpiFileProcessor:
     def __init__(self, input_file):
@@ -22,29 +23,54 @@ class SpiFileProcessor:
 
     def process_spi(self):
         with open(self.input_file, 'r') as infile:
+            saved_spi_writes = set()
             for line in infile:
                 if line.startswith("spi_write"):
                     self.spi_write_data += line
                 elif line.startswith("spi_read"):
                     key, encrypted_data = self.encrypt(self.spi_write_data)
                     self.save_to_db(key, encrypted_data, line)
+                    saved_spi_writes.add(self.spi_write_data)
                     self.spi_write_data = ""
+            # Save the remaining spi_write_data if it has not been saved yet
+            if self.spi_write_data and self.spi_write_data not in saved_spi_writes:
+                key, encrypted_data = self.encrypt(self.spi_write_data)
+                self.save_to_db(key, encrypted_data, "")
+
 
     def save_to_db(self, key, encrypted_data, spi_read_line):
         conn = sqlite3.connect("spi_data.db")
         cursor = conn.cursor()
+
+        # Create a hash of the spi_write and spi_read lines to use as the table name
+        table_name = "spi_" + hashlib.sha1((self.spi_write_data + spi_read_line).encode()).hexdigest()
+
         cursor.execute(
-            "CREATE TABLE IF NOT EXISTS spi_data (encryption_key BLOB, encrypted_value BLOB, spi_read_line TEXT UNIQUE)"
+            f"""CREATE TABLE IF NOT EXISTS {table_name} (
+                encryption_key BLOB, encrypted_value BLOB, spi_write_line TEXT, spi_read_line TEXT, entry_count INTEGER
+            )"""
         )
 
-        cursor.execute("SELECT COUNT(*) FROM spi_data WHERE spi_read_line = ?", (spi_read_line,))
-        count = cursor.fetchone()[0]
+        cursor.execute(f"SELECT COUNT(*), COALESCE(entry_count, 0) FROM {table_name} WHERE spi_write_line = ?", (self.spi_write_data,))
+        count, entry_count = cursor.fetchone()
 
         if count == 0:
-            cursor.execute("INSERT INTO spi_data (encryption_key, encrypted_value, spi_read_line) VALUES (?, ?, ?)", (key, encrypted_data, spi_read_line))
-            conn.commit()
+            cursor.execute(
+                f"""INSERT INTO {table_name} (encryption_key, encrypted_value, spi_write_line, spi_read_line, entry_count)
+                VALUES (?, ?, ?, ?, ?)""",
+                (key, encrypted_data, self.spi_write_data, spi_read_line, 1),
+            )
+        else:
+            entry_count += 1
+            cursor.execute(
+                f"""UPDATE {table_name} SET encryption_key = ?, encrypted_value = ?, spi_read_line = ?, entry_count = ?
+                WHERE spi_write_line = ?""",
+                (key, encrypted_data, spi_read_line, entry_count, self.spi_write_data),
+            )
 
+        conn.commit()
         conn.close()
+
 
 class SQLiteDatabase:
     def __init__(self, db_path):
@@ -69,9 +95,11 @@ def main(input_file, decrypt_flag=False):
         db = SQLiteDatabase("spi_data.db")
         rows = db.read_table("spi_data")
         for row in rows:
+            print("\n****************")
             key, encrypted_data, spi_read_line = row
             decrypted_data = spi_processor.decrypt(key, encrypted_data)
-            print(f"{spi_read_line.strip()} -> {decrypted_data}")
+            print(f"{decrypted_data}->\n{spi_read_line.strip()}")
+            print("****************\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process input file.")
