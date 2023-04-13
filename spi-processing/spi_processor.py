4410b377273e9a8c462f9ba1590e7fe2861e9bca
onur <onurulusoys4@gmail.com>
2023-04-13 13:45:41 +0300

    Implement planned tree structure to spi_processor

--- spi-processing/spi_processor.py
+++ spi-processing/spi_processor.py
@@ -55,6 +55,28 @@ class SpiFileProcessor:
         with open(json_file, 'r') as f:
             self.spi_data = json.load(f)
 
+        self.spi_tree = {}
+        for table_name, rows in self.spi_data.items():
+            for i, row in enumerate(rows):
+                spi_write_line, spi_read_line, entry_count = row.values()
+                if spi_write_line not in self.spi_tree:
+                    self.spi_tree[spi_write_line] = []
+                iteration_number = 1 if i == 0 else 0
+                self.spi_tree[spi_write_line].append((spi_read_line, entry_count, iteration_number))
+
+    def print_spi_tree(self):
+        # ANSI escape codes for colors
+        GREEN = "\033[32m"
+        RESET = "\033[0m"
+        YELLOW = "\033[33m"
+
+        for spi_write_line, associated_spi_read_lines in self.spi_tree.items():
+            print(f"{spi_write_line}:")
+            for spi_read_line, entry_count, iteration_number in associated_spi_read_lines:
+                print(f"  {spi_read_line} {GREEN}x{entry_count}, {YELLOW} iteration: {iteration_number}{RESET}")
+            print()
+
+
     def display_spi_data(self):
         # ANSI escape codes for colors
         GREEN = "\033[32m"
@@ -77,27 +99,61 @@ class SpiFileProcessor:
                 print(f"{colored_spi_write_data}\nassociates\n{colored_spi_read_line}\n\n(entry count: {entry_count})")
                 print("\n")
 
+# class SpiReadFinder:
+#     def __init__(self, json_file):
+#         self.json_file = json_file
+#         self.spi_data = {}
+
+#     def load_spi_data(self):
+#         with open(self.json_file, 'r') as infile:
+#             self.spi_data = json.load(infile)
+
+#     def find_spi_read_line(self, spi_write_data):
+#         self.load_spi_data()
+
+#         table_name = encrypt_write_data(spi_write_data)
+
+#         if table_name not in self.spi_data:
+#             return None
+
+#         rows = self.spi_data[table_name]
+#         for row in rows:
+#             if row["spi_write_line"] == spi_write_data:
+#                 return row["spi_read_line"]
+
+#         return None
+    
 def encrypt_write_data(spi_write_data):
     # Create a hash of the spi_write line to use as the table name
     table_name = "spi_" + hashlib.sha1(spi_write_data.encode()).hexdigest()
     return table_name
 
-def main(input_file, display_flag=False):
+def main(input_file, display_flag=False, find_flag=False, print_tree_flag=False):
     spi_processor = SpiFileProcessor(input_file)
-    if not display_flag:
+
+    if not display_flag and not find_flag and not print_tree_flag and input_file is not None:
         spi_processor.process_spi()
-    else:
+    elif display_flag:
         spi_processor.display_spi_data()
+    elif find_flag:
+        spi_processor.create_tree_from_json()
+        ##
+    elif print_tree_flag:
+        spi_processor.create_tree_from_json()
+        spi_processor.print_spi_tree()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process input file.")
     parser.add_argument("-i", "--input", help="The input text file.", default=None)
     parser.add_argument("-d", "--display", help="Display the SPI data.", action="store_true")
-    
+    parser.add_argument("-f", "--find", help="Find the associated SPI read line.", action="store_true")
+    parser.add_argument("-p", "--print-tree", help="Print the SPI tree.", action="store_true")
+
     args = parser.parse_args()
 
-    if not args.display and args.input is None:
-        print("Error: An input file is required when not using the --display flag.")
+    if not args.display and not args.find and not args.print_tree and args.input is None:
+        print("Error: An input file is required when not using the --display, --find, or --print-tree flags.")
         parser.print_help()
     else:
-        main(args.input, args.display)
+        main(args.input, args.display, args.find, args.print_tree)
+
