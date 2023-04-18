2611ee64e8f4eea13ad80dd5b49e7f5dc82b5035
Onur Ulusoy <onurulusoys4@gmail.com>
2023-04-18 17:36:01 +0300

    Enhance SpiDevRequest class to generate and handle processed JSON file

--- spi-processing/cpp_wrapper/src/SpiProcessorUtil.cpp
+++ spi-processing/cpp_wrapper/src/SpiProcessorUtil.cpp
@@ -8,13 +8,13 @@ class SpiDevEntry {
 public:
     std::string device_type;
     int offset_number;
-    std::vector<std::vector<std::string>> spi_write; 
+    std::vector<std::vector<std::string>> spi_write;
 
     void print() const {
         std::cout << "Device Type: " << device_type << std::endl;
         std::cout << "Offset Number: " << offset_number << std::endl;
         std::cout << "SPI Write: " << std::endl;
-        for (const auto& group : spi_write) { 
+        for (const auto& group : spi_write) {
             for (const auto& write : group) {
                 std::cout << "  " << write << std::endl;
             }
@@ -33,7 +33,7 @@ public:
         std::string line;
         nlohmann::json output;
 
-        output["device_type"] = m_device;
+        output["device_type"] = dev_type;
         output["offset_number"] = offset;
 
         std::vector<std::vector<std::string>> spi_write_groups;
@@ -82,6 +82,70 @@ public:
         return devEntry;
     }
 
+    SpiDevEntry parse_processed_json_file() {
+        std::ifstream infile(raw_filename + "_processed" + ".json");
+        nlohmann::json input;
+
+        infile >> input;
+
+        devEntry.device_type = input["device_type"];
+        devEntry.offset_number = input["offset_number"];
+
+        for (const auto &group : input["spi_write"]) {
+            std::vector<std::string> current_group;
+            for (const auto &write_pair : group) {
+                for (const auto &write : write_pair) {
+                    current_group.push_back(write);
+                }
+            }
+            devEntry.spi_write.push_back(current_group);
+        }
+
+        return devEntry;
+    }
+    
+    void process_and_save_json() {
+        nlohmann::json input, output;
+        std::ifstream infile(raw_filename + ".json");
+        infile >> input;
+
+        output["device_type"] = input["device_type"];
+        output["offset_number"] = input["offset_number"];
+
+        std::vector<std::vector<std::vector<std::string>>> spi_write_groups;
+        for (const auto &group : input["spi_write"]) {
+            std::vector<std::vector<std::string>> current_group;
+            for (const auto &write : group) {
+                std::stringstream ss(write.get<std::string>());
+                std::string temp;
+                std::vector<std::string> tokens;
+
+                while (ss >> temp) {
+                    tokens.push_back(temp);
+                }
+
+                int data_size = std::stoi(tokens[3].substr(0, tokens[3].find(':')));
+                std::string buffer = tokens[5];
+
+                for (size_t i = 6; i < tokens.size(); i++) {
+                    buffer += " " + tokens[i];
+                }
+
+                std::string new_write_1 = "write-" + output["device_type"].get<std::string>() + "device-" + std::to_string(output["offset_number"].get<int>()) + "-buffer-" + buffer;
+                std::string new_write_2 = "write-" + output["device_type"].get<std::string>() + "device-" + std::to_string(output["offset_number"].get<int>()) + "-data_size-" + std::to_string(data_size);
+
+                std::vector<std::string> write_pair = {new_write_1, new_write_2};
+                current_group.push_back(write_pair);
+            }
+            spi_write_groups.push_back(current_group);
+        }
+
+        output["spi_write"] = spi_write_groups;
+
+        std::ofstream outfile(raw_filename + "_processed" + ".json");
+        outfile << output.dump(4);
+    }
+
     SpiDevEntry getDevEntry() const {
         return devEntry;
     }
@@ -90,7 +154,7 @@ private:
     std::string filename;
     std::string raw_filename;
 
-    const std::string m_device = "spi";
+    const std::string dev_type = "spi";
     int offset;      
     SpiDevEntry devEntry;
 
@@ -110,6 +174,9 @@ int main() {
     SpiDevRequest spi("SPI_A.txt", 0);
     spi.raw_to_json();
     spi.parse_json_file();
+    spi.getDevEntry().print();
+    spi.process_and_save_json();
+    spi.parse_processed_json_file();
     spi.getDevEntry().print();
 
     return 0;
