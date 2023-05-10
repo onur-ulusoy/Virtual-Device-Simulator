6cd30590d53d4f91e5c0839e68478d6d44d56b19
Onur Ulusoy <onurulusoys4@gmail.com>
2023-05-10 23:04:02 +0300

    Setup CMakeLists and workspace for Mock, further develop the class

--- program_tested_mock/src/ProgramToBeTested.cpp
+++ program_tested_mock/src/ProgramToBeTested.cpp
@@ -8,12 +8,16 @@
 #include <nlohmann/json.hpp>
 #include <chrono>
 #include <thread>
+#include <filesystem>
+
+//#include <ProgramToBeTested.hpp>
 
 class ProgramToBeTested {
 public:
     ProgramToBeTested(const std::string &spi_data_json)
         : spi_data_json_(spi_data_json) {
-            std::ofstream verification_file_check("verification.txt");
+            createLogsFolder();
+            std::ofstream verification_file_check("verification");
         }
 
     void GenerateRandomSamples() {
@@ -65,7 +69,7 @@ public:
         // Check if the "verification.txt" file exists, and create it if not
         std::ifstream verification_file_check("verification");
         if (!verification_file_check.is_open()) {
-            std::ofstream create_verification_file("verification.txt");
+            std::ofstream create_verification_file("verification");
             create_verification_file.close();
         } else {
             verification_file_check.close();
@@ -75,18 +79,16 @@ public:
         while (true) {
             std::ifstream verification_file("verification");
             if (!verification_file.is_open()) {
-                std::cerr << "Failed to open verification file: verification.txt" << std::endl;
+                std::cerr << "Failed to open verification file: verification" << std::endl;
                 return false;
             }
             getline(verification_file, input);
             verification_file.close();
 
             if (input == "OK") {
-                std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                std::ofstream output_file("verification.txt");
+                moveLogFile();
                 return true;
-            }
-                
+            }         
 
             else if (input == "EXIT") 
                 return false;
@@ -94,6 +96,55 @@ public:
             else   
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
+        }
+    }
+
+    void moveLogFile()
+    {        
+        // Wait for 500ms before proceeding with the file operations
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        
+        // Define the name of the log file and its new location
+        std::string logFileName = "SPI_Log.txt";
+        std::string logsFolder = "Logs/";
+        std::string dateStr = std::to_string(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
+        
+        // Check if the log file exists
+        if (!std::filesystem::exists(logFileName)) {
+            std::cerr << "Failed to find log file: " << logFileName << std::endl;
+            exit(-1);
+        }
+        
+        // Rename the log file with the current date
+        std::string newLogFileName = logFileName + "_" + dateStr;
+        std::cout << "SPI_Log saved successfully." << std::endl;
+        std::filesystem::rename(logFileName, newLogFileName);
+
+        // Move the renamed log file to the /Logs folder
+        std::filesystem::create_directory(logsFolder);
+        std::filesystem::rename(newLogFileName, logsFolder + newLogFileName);
+
+        // Create a verification file to indicate that the operation was successful
+        std::ofstream output_file("verification");
+    }
+
+    void createLogsFolder() {
+        // Define the name of the logs folder
+        std::string logsFolder = "Logs/";
+
+        // Check if the folder already exists
+        if (std::filesystem::exists(logsFolder)) {
+            //std::cout << "Logs folder already exists." << std::endl;
+            return;
+        }
+
+        // Create the folder
+        try {
+            std::filesystem::create_directory(logsFolder);
+            //std::cout << "Logs folder created successfully." << std::endl;
+        }
+        catch (std::filesystem::filesystem_error& e) {
+            std::cerr << "Failed to create Logs folder: " << e.what() << std::endl;
         }
     }
 
