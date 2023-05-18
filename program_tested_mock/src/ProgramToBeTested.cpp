a8a0208966d786431e796e47b49925e05d339a15
Onur Ulusoy <onurulusoys4@gmail.com>
2023-05-19 02:47:32 +0300

    Separate header and source files for ProgramToBeTested class, add Doxygen comments

--- program_tested_mock/src/ProgramToBeTested.cpp
+++ program_tested_mock/src/ProgramToBeTested.cpp
@@ -1,161 +1,139 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
-#include <random>
-#include <nlohmann/json.hpp>
-#include <chrono>
-#include <thread>
-#include <filesystem>
+/**
+ * @file ProgramToBeTested.cpp
+ * @brief Contains the definition of the ProgramToBeTested class and main() function.
+ *
+ * The ProgramToBeTested class serves as a mock structure of a program to be tested.
+ * It simulates the interactions with SPI devices by generating random SPI commands and storing logs of the communication for analysis.
+ * 
+ * @date 18.05.2023
+ * @section LICENSE
+ * Licensed under the MIT License.
+ */
 
-//#include <ProgramToBeTested.hpp>
-
-class ProgramToBeTested {
-public:
-    ProgramToBeTested(const std::string &spi_data_json)
-        : spi_data_json_(spi_data_json) {
-            createLogsFolder();
-            std::ofstream verification_file_check("verification");
-        }
-
-    void GenerateRandomSamples() {
-        // Read input JSON file and store all spi_write lines in the vector
-        nlohmann::json spi_data;
-        std::ifstream input_file(spi_data_json_);
-        if (!input_file.is_open()) {
-            std::cerr << "Failed to open input file: " << spi_data_json_ << std::endl;
-            return;
-        }
-
-        input_file >> spi_data;
-        input_file.close();
-
-        std::vector<std::string> spi_write_lines;
-        for (const auto &trunk : spi_data.items()) {
-            for (const auto &entry : trunk.value()) {
-                spi_write_lines.push_back(entry["spi_write_line"].get<std::string>());
-            }
-        }
-
-        // Shuffle the spi_write lines
-        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-        std::shuffle(spi_write_lines.begin(), spi_write_lines.end(), std::default_random_engine(seed));
-
-        // Choose a random number of lines to write
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<size_t> dist(1, spi_write_lines.size());
-        size_t num_lines_to_write = dist(gen);
-
-        // Write the randomly selected lines to the output file
-        std::ofstream output_file("SPI_A.txt");
-        if (!output_file.is_open()) {
-            std::cerr << "Failed to open output file: SPI_A.txt" << std::endl;
-            return;
-        }
-
-        for (size_t i = 0; i < num_lines_to_write; ++i) {
-            output_file << spi_write_lines[i] << std::endl;
-            output_file << std::endl;
-        }
-        output_file.close();
-    }
-
-    bool WaitForVerification() {
-        std::string input;
-
-        // Check if the "verification.txt" file exists, and create it if not
-        std::ifstream verification_file_check("verification");
-        if (!verification_file_check.is_open()) {
-            std::ofstream create_verification_file("verification");
-            create_verification_file.close();
-        } else {
-            verification_file_check.close();
-        }
-
-        // Continuously read input from "verification.txt" file until a valid input is found
-        while (true) {
-            std::ifstream verification_file("verification");
-            if (!verification_file.is_open()) {
-                std::cerr << "Failed to open verification file: verification" << std::endl;
-                return false;
-            }
-            getline(verification_file, input);
-            verification_file.close();
-
-            if (input == "OK") {
-                moveLogFile();
-                return true;
-            }         
-
-            else if (input == "EXIT"){
-                moveLogFile();
-                return false;
-            }
-                
-            else   
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        }
-    }
-
-    void moveLogFile()
-    {        
-        // Wait for 500ms before proceeding with the file operations
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        
-        // Define the name of the log file and its new location
-        std::string logFileName = "SPI_Log.txt";
-        std::string logsFolder = "Logs/";
-        std::string dateStr = std::to_string(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
-        
-        // Check if the log file exists
-        if (!std::filesystem::exists(logFileName)) {
-            std::cerr << "Failed to find log file: " << logFileName << std::endl;
-            exit(-1);
-        }
-        
-        // Rename the log file with the current date
-        std::string newLogFileName = logFileName + "_" + dateStr;
-        std::cout << "SPI_Log saved successfully." << std::endl;
-        std::filesystem::rename(logFileName, newLogFileName);
-
-        // Move the renamed log file to the /Logs folder
-        std::filesystem::create_directory(logsFolder);
-        std::filesystem::rename(newLogFileName, logsFolder + newLogFileName);
-
-        // Create a verification file to indicate that the operation was successful
-        std::ofstream output_file("verification");
-    }
-
-    void createLogsFolder() {
-        // Define the name of the logs folder
-        std::string logsFolder = "Logs/";
-
-        // Check if the folder already exists
-        if (std::filesystem::exists(logsFolder)) {
-            //std::cout << "Logs folder already exists." << std::endl;
-            return;
-        }
-
-        // Create the folder
-        try {
-            std::filesystem::create_directory(logsFolder);
-            //std::cout << "Logs folder created successfully." << std::endl;
-        }
-        catch (std::filesystem::filesystem_error& e) {
-            std::cerr << "Failed to create Logs folder: " << e.what() << std::endl;
-        }
-    }
-
-private:
-    std::string spi_data_json_;
-};
-
+#include <ProgramToBeTested.hpp>
 #include <unistd.h>
 
+ProgramToBeTested::ProgramToBeTested(const std::string& spi_data_json)
+    : spi_data_json_(spi_data_json) {
+    createLogsFolder();
+    std::ofstream verification_file_check("verification");
+}
+
+void ProgramToBeTested::GenerateRandomSamples() {
+    nlohmann::json spi_data;
+    std::ifstream input_file(spi_data_json_);
+    if (!input_file.is_open()) {
+        std::cerr << "Failed to open input file: " << spi_data_json_ << std::endl;
+        return;
+    }
+
+    input_file >> spi_data;
+    input_file.close();
+
+    std::vector<std::string> spi_write_lines;
+    for (const auto& trunk : spi_data.items()) {
+        for (const auto& entry : trunk.value()) {
+            spi_write_lines.push_back(entry["spi_write_line"].get<std::string>());
+        }
+    }
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(spi_write_lines.begin(), spi_write_lines.end(), std::default_random_engine(seed));
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<size_t> dist(1, spi_write_lines.size());
+    size_t num_lines_to_write = dist(gen);
+
+    std::ofstream output_file("SPI_A.txt");
+    if (!output_file.is_open()) {
+        std::cerr << "Failed to open output file: SPI_A.txt" << std::endl;
+        return;
+    }
+
+    for (size_t i = 0; i < num_lines_to_write; ++i) {
+        output_file << spi_write_lines[i] << '\n';
+        output_file << '\n';
+    }
+
+    output_file.close();
+}
+
+bool ProgramToBeTested::WaitForVerification() {
+    std::string input;
+
+    std::ifstream verification_file_check("verification");
+    if (!verification_file_check.is_open()) {
+        std::ofstream create_verification_file("verification");
+        create_verification_file.close();
+    } else {
+        verification_file_check.close();
+    }
+
+    while (true) {
+        std::ifstream verification_file("verification");
+        if (!verification_file.is_open()) {
+            std::cerr << "Failed to open verification file: verification" << std::endl;
+            return false;
+        }
+        getline(verification_file, input);
+        verification_file.close();
+
+        if (input == "OK") {
+            moveLogFile();
+            return true;
+        } else if (input == "EXIT") {
+            moveLogFile();
+            return false;
+        } else
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void ProgramToBeTested::moveLogFile() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::string logFileName = "SPI_Log.txt";
+    std::string logsFolder = "Logs/";
+    std::string dateStr = std::to_string(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
+
+    if (!std::filesystem::exists(logFileName)) {
+        std::cerr << "Failed to find log file: " << logFileName << std::endl;
+        exit(-1);
+    }
+
+    std::string newLogFileName = logFileName + "_" + dateStr;
+    std::cout << "SPI_Log saved successfully." << std::endl;
+    std::filesystem::rename(logFileName, newLogFileName);
+
+    std::filesystem::create_directory(logsFolder);
+    std::filesystem::rename(newLogFileName, logsFolder + newLogFileName);
+
+    std::ofstream output_file("verification");
+}
+
+void ProgramToBeTested::createLogsFolder() {
+    std::string logsFolder = "Logs/";
+
+    if (std::filesystem::exists(logsFolder)) {
+        return;
+    }
+
+    try {
+        std::filesystem::create_directory(logsFolder);
+    } catch (std::filesystem::filesystem_error& e) {
+        std::cerr << "Failed to create Logs folder: " << e.what() << std::endl;
+    }
+}
+
+ /**
+  * @brief The main entry point for the testing program.
+  *
+  * This function constructs the ProgramToBeTested object and initiates the generation of random samples.
+  * It then waits for verification. If the verification returns false, it breaks the loop and ends the program.
+  *
+  * @return 0 upon successful execution.
+  */
 int main() {
     char cwd[256];
     if (getcwd(cwd, sizeof(cwd)) != nullptr) {
